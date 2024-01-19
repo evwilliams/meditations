72cde178df76f6a2017713648801fb714dba0394
evwilliams <evanw6@gmail.com>
2024-01-19 11:33:29 -0800

    Some textarea focus hackery

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,3 +1,15 @@
+declare global {
+  interface Window {
+    isMobileDevice: () => boolean
+  }
+}
+
+window.isMobileDevice = () => {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+}
+
 export const randomID = () =>
   Date.now().toString(16) + Math.random().toString(16).slice(2)
 
--- src/components/ThoughtPad.tsx
+++ src/components/ThoughtPad.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react'
 import { Thought } from '../data/Thought'
 
 type UpdateHandler = (id: number, text: string) => void
@@ -9,9 +10,27 @@ type ThoughtPadProps = {
 
 const ThoughtPad = ({ thought, onUpdate, ...rest }: ThoughtPadProps) => {
 
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  useEffect(() => {
+    focusTextArea()
+  }, [])
+
+  useEffect(() => {
+    if (thought.text == '')
+      focusTextArea()
+  }, [thought])
+
+  const focusTextArea = () => {
+    if (!textareaRef.current || !window.isMobileDevice()) return
+    textareaRef.current.focus()
+    textareaRef.current.selectionStart = textareaRef.current.selectionEnd = thought.text.length;
+  }
+
   return (
     <textarea
       id='thought-pad'
+      ref={textareaRef}
       className={rest.className}
       placeholder="What's on your mind?"
       value={thought.text}
