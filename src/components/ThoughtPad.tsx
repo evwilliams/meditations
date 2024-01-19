4ac72b149a84213c53477579577461cc6b5320fe
evwilliams <evanw6@gmail.com>
2024-01-19 11:03:18 -0800

    Update content

--- src/components/ThoughtPad.tsx
+++ src/components/ThoughtPad.tsx
@@ -1,8 +1,5 @@
 import { Thought } from '../data/Thought'
 
-const placeholder =
-  'Whenever you are about to find fault with someone, ask yourself the following question: What fault of mine most nearly resembles the one I am about to criticize?'
-
 type UpdateHandler = (id: number, text: string) => void
 
 type ThoughtPadProps = {
@@ -16,7 +13,7 @@ const ThoughtPad = ({ thought, onUpdate, ...rest }: ThoughtPadProps) => {
     <textarea
       id='thought-pad'
       className={rest.className}
-      placeholder={placeholder}
+      placeholder="What's on your mind?"
       value={thought.text}
       onChange={(e) => thought.id && onUpdate(thought.id, e.target.value)}
     />
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,17 +1,15 @@
 type WelcomeProps = {} & React.HTMLAttributes<HTMLDivElement>
 
 const Welcome = ({ ...rest }: WelcomeProps) => {
-	const lines = [
-		`A simple tool for "repeating and reframing ideas long familiar but imperfectly absorbed", inspired by Marcus Aurelius' practice of writing found in Meditations.`,
-		`There's no server, all your notes are local to this browser.`,
-		`Unlike a notepad, this will resurface past thoughts so you can reflect and refine.`,
-		`Press the + button below to start collecting your thoughts.`,
-	]
 	return (
-		<div {...rest}
-			className={`${rest.className} + leading-relaxed flex flex-col gap-2`}>
+		<div {...rest}>
 			<header className="text-2xl italic">Welcome to Meditations</header>
-			{lines.map(l => <p>{l}</p>)}
+			<div className="leading-relaxed flex flex-col gap-4 py-4 text-neutral-500">
+				<p>A simple tool for <em className='text-neutral-900'>"repeating and reframing ideas long familiar but imperfectly absorbed"</em>, inspired by Marcus Aurelius' practice of writing found in Meditations.</p>
+				<p>There's <em className='text-neutral-900'>no server</em>, all your notes are <em className='text-neutral-900'>private & stored locally in this browser.</em></p>
+				<p>Unlike a notepad, this will <em className='text-neutral-900'>resurface past thoughts</em> so you can reflect and refine.</p>
+				<p><em className='text-neutral-900'>Press the + button below</em> to start collecting your thoughts.</p>
+			</div>
 		</div>
 	)
 }
