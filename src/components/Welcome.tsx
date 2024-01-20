9c2a584b4fb43f67a29f912c93ecf460c7195027
evwilliams <evanw6@gmail.com>
2024-01-19 16:16:25 -0800

    Tweaking welcome text

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -5,9 +5,10 @@ const Welcome = ({ ...rest }: WelcomeProps) => {
 		<div {...rest}>
 			<header className="text-2xl italic">Welcome to Meditations</header>
 			<div className="leading-relaxed flex flex-col gap-8 py-8 text-neutral-500 text-lg">
-				<p>A simple tool for <em className='text-neutral-900'>"repeating and reframing ideas long familiar but imperfectly absorbed"</em>, inspired by Marcus Aurelius' Meditations.</p>
+				<p>Inspired by Marcus Aurelius.</p>
+				<p>A simple tool for <em className='text-neutral-900'>"repeating and reframing ideas long familiar but imperfectly absorbed".</em></p>
 				<p>There's <em className='text-neutral-900'>no server</em>, all your notes are <em className='text-neutral-900'>private & stored locally in this browser.</em></p>
-				<p>Unlike a notepad, this will <em className='text-neutral-900'>resurface past thoughts</em> so you can reflect and refine.</p>
+				<p>Unlike a notepad, this will <em className='text-neutral-900'>help resurface past thoughts</em>.</p>
 				<p><em className='text-neutral-900'>Press the + button below</em> to start collecting your thoughts.</p>
 			</div>
 		</div>
